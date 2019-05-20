ff5bccc49ad8a003481fc7f7248902640e26b20f
Axl Joven <axlmorenojoven@gmail.com>
2019-05-20 19:43:06 +0800

    Changed the tab sizes of Registrant.php and single.php

--- application/controllers/Registrant.php
+++ application/controllers/Registrant.php
@@ -2,54 +2,79 @@
 
 class Registrant extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->helper(array('form', 'url'));
-    }
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    public function register($event_id) 
-    {
-        $this->load->helper('form');
-        $this->load->library('form_validation');
-        
-        $this->form_validation->set_rules('lastname',       'lastname',                     'trim|required|min_length[2]|max_length[32]|alpha');
-        $this->form_validation->set_rules('firstname',      'firstname',                    'trim|required|min_length[2]|max_length[32]|alpha');
-        $this->form_validation->set_rules('middlename',     'middle name',                  'trim|required|min_length[2]|max_length[32]|alpha');
-        $this->form_validation->set_rules('gender',         'gender',                       'trim|required');
-        $this->form_validation->set_rules('email',          'email',                        'trim|required|valid_email');
-        $this->form_validation->set_rules('mobile-number',  'mobile number',                'trim|required|integer|min_length[4]|numeric');
-        $this->form_validation->set_rules('church-name',    'church / organization name',   'trim|required|min_length[2]|max_length[100]');
-        $this->form_validation->set_rules('church-city',    'church / organization city',   'trim|required|min_length[2]|max_length[100]');
-        $this->form_validation->set_rules('role',           'role',                         'trim|required|min_length[2]|max_length[6]');
-        
-        if ($this->form_validation->run() == FALSE) {
-            // validation failed
-            $this->load->model('event_model');
-            $data['event'] = $this->event_model->fetch($event_id);
-            $data['title'] = "Event | " .$data['event']['title'];
-            
-            // Load view
-            $this->load->view('templates/header', $data);
-            $this->load->view('pages/events/single', $data);
-            $this->load->view('templates/footer', $data);
-        
-        } else {
-            // validation is a success 
-            $this->load->model('event_model');
-            $data['event'] = $this->event_model->fetch($event_id);
-            $data['title'] = "Registered | ".$data['event']['title'];
-            $data['name'] = $data['event']['title'];
-            $data['id'] = $data['event']['id'];
-            
-            // Send email
+	//
+	// Register
+	//
 
-            // Save to database
+	public function register($event_id) 
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('lastname',       'lastname',                     'trim|required|min_length[2]|max_length[32]|alpha');
+		$this->form_validation->set_rules('firstname',      'firstname',                    'trim|required|min_length[2]|max_length[32]|alpha');
+		$this->form_validation->set_rules('middlename',     'middle name',                  'trim|required|min_length[2]|max_length[32]|alpha');
+		$this->form_validation->set_rules('gender',         'gender',                       'trim|required');
+		$this->form_validation->set_rules('email',          'email',                        'trim|required|valid_email');
+		$this->form_validation->set_rules('mobile-number',  'mobile number',                'trim|required|integer|min_length[4]|numeric');
+		$this->form_validation->set_rules('church-name',    'church / organization name',   'trim|required|min_length[2]|max_length[100]');
+		$this->form_validation->set_rules('church-city',    'church / organization city',   'trim|required|min_length[2]|max_length[100]');
+		$this->form_validation->set_rules('role',           'role',                         'trim|required|min_length[2]|max_length[6]');
+		
+		if ($this->form_validation->run() == FALSE) {
+			// validation failed
+			$this->load->model('event_model');
+			$data['event'] = $this->event_model->fetch($event_id);
+			$data['title'] = "Event | " .$data['event']['title'];
+			
+			// Load view
+			$this->load->view('templates/header', $data);
+			$this->load->view('pages/events/single', $data);
+			$this->load->view('templates/footer', $data);
+		
+		} else {
+			// validation is a success 
+			$this->load->model('event_model');
+			$data['event'] = $this->event_model->fetch($event_id);
+			$data['title'] = "Registered | ".$data['event']['title'];
+			$data['name'] = $data['event']['title'];
+			$data['id'] = $data['event']['id'];
+			
+			// Send email
+			$this->send_confirm_email();
 
-            // Load view
-            $this->load->view('templates/header', $data);
-            $this->load->view('pages/events/reg-success', $data);
-            $this->load->view('templates/footer', $data);
-        }
-    }
+			// Save to database
+
+			// Load view
+			$this->load->view('templates/header', $data);
+			$this->load->view('pages/events/reg-success', $data);
+			$this->load->view('templates/footer', $data);
+		}
+    	}
+
+	//
+	// Send confirmation email
+	//
+
+	private function send_confirm_email()
+	{
+		$this->load->library('email');
+
+		$this->email->from('your@example.com', 'NeedGod Ministries');
+		$this->email->to('axlmorenojoven@gmail.com');
+		$this->email->cc('axl_joven@yahoo.com');
+		// $this->email->bcc('them@their-example.com');
+
+		$this->email->subject('Email Test');
+		$this->email->message('Testing the email class.');
+
+		$this->email->send();
+
+		// Check if email sending is successful
+	}
 }
--- application/views/pages/events/single.php
+++ application/views/pages/events/single.php
@@ -7,42 +7,51 @@
 ?>
 
 <div>
-    <div class="container mx-auto px-6">
-        <article class="mb-8 pb-6 border-b border-b-gray-100">
-            <!-- Title -->
-            <h1 class="text-3xl font-bold mb-4"><?php echo $event['title'] ?></h1>
-            
-            <!-- Content -->
-            <div class="event-card-details mb-6">
-                <?php echo $event['details'] ?>
-            </div>
+	<div class="container mx-auto px-6">
+		<article class="mb-8 pb-6 border-b border-b-gray-100">
+			<!-- Title -->
+			<h1 class="text-3xl font-bold mb-4"><?php echo $event['title'] ?></h1>
 
-            <!-- Speakers -->
-            <div class="mb-4">
-                <h2 class="font-bold mb-1 text-sm"><i class="fas fa-user-friends fa-fw fa-lg text-red-600 mr-2"></i> Speaker(s)</h2>
-                <?php echo $event['speakers'] ?>
-            </div>
+			<!-- Content -->
+			<div class="event-card-details mb-6">
+				<?php echo $event['details'] ?>
+			</div>
 
-            <!-- Reg fee -->
-            <div class="mb-4">
-                <h2 class="font-bold mb-1 text-sm"><i class="fas fa-money-bill-alt fa-fw fa-lg text-red-600 mr-2"></i> Registration fee</h2>
-                <?php echo $event['reg_fee_details'] ?>
-            </div>
-            
-            <!-- Venue -->
-            <div class="mb-4">
-                <h2 class="font-bold mb-1 text-sm"><i class="far fa-map fa-fw fa-lg text-red-600 mr-2"></i> Venue</h2>
-                <?php echo $event['venue'] ?>
-            </div>
+			<!-- Speakers -->
+			<div class="mb-4">
+				<h2 class="font-bold mb-1 text-sm">
+					<i class="fas fa-user-friends fa-fw fa-lg text-red-600 mr-2"></i> Speaker(s)
+				</h2>
+				<p class=""><?php echo $event['speakers'] ?></p>
+			</div>
 
-            <!-- Time and Date -->
-            <div class="mb-4">
-                <h2 class="font-bold mb-1 text-sm"><i class="far fa-calendar-alt fa-fw fa-lg text-red-600 mr-2"></i> Date and Time</h2>
-                <?php echo $event['date']; ?>
-            </div>
-        </article>
+			<!-- Reg fee -->
+			<div class="mb-4">
+				<h2 class="font-bold mb-1 text-sm">
+					<i class="fas fa-money-bill-alt fa-fw fa-lg text-red-600 mr-2"></i> Registration
+					fee
+				</h2>
+				<p class=""><?php echo $event['reg_fee_details'] ?></p>
+			</div>
 
-        <!-- Registration form -->
-        <?php include('reg-form.php') ?>
-    </div>
+			<!-- Venue -->
+			<div class="mb-4">
+				<h2 class="font-bold mb-1 text-sm">
+					<i class="far fa-map fa-fw fa-lg text-red-600 mr-2"></i> Venue
+				</h2>
+				<p class=""><?php echo $event['venue'] ?></p>
+			</div>
+
+			<!-- Time and Date -->
+			<div class="mb-4">
+				<h2 class="font-bold mb-1 text-sm">
+					<i class="far fa-calendar-alt fa-fw fa-lg text-red-600 mr-2"></i> Date and Time
+				</h2>
+				<p class=""><?php echo $event['date']; ?></p>
+			</div>
+		</article>
+
+		<!-- Registration form -->
+		<?php include('reg-form.php') ?>
+	</div>
 </div>
