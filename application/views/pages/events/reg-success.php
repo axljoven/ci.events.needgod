fa6269f5c5334cc38271d516706cdad2a9f5a007
Axl Joven <axlmorenojoven@gmail.com>
2019-05-20 19:14:05 +0800

    Created the registration success page with a link to go back to the event

---
+++ application/views/pages/events/reg-success.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="container mx-auto px-6">
+        <h1 class="text-xl">
+            You are now registered to <br/>
+            <span class="text-2xl font-bold"><?php echo $name ?>!</span>
+        </h1>
+        <p>Check your email for confirmation. <span class="font-bold"><?php echo anchor('event/'.$id, 'Go back'); ?></span></p>
+    </div>
+</div>
