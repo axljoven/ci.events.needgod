4de74208a994c7356ca34d55ebcd09572dffa2dc
Axl Joven <axlmorenojoven@gmail.com>
2019-05-21 16:20:15 +0800

    Added the create() function inside the Registrant model that saves the new registrant entry to the database

--- application/models/registrant_model.php
+++ application/models/registrant_model.php
@@ -1,52 +1,48 @@
 <?php
 
-class Registrants_model extends CI_Model
+class Registrant_model extends CI_Model
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->database();
-    }
-    
-    // 
-    // Fetch all registrants or by ID
-    // 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+	
+	// 
+	// Fetch all registrants or by ID
+	// 
 
-    public function fetch($id = NULL)
-    {
-        if ($id == NULL) :
-            $result = $this->db->get('registrants');
-            return $result->result_array();
-        endif;
+	public function fetch($id = NULL)
+	{
+		if ($id == NULL) :
+			$result = $this->db->get('registrants');
+			return $result->result_array();
+		endif;
 
-        $result = $this->db->get_where('registrants', ['id' => $id]);
-        return $result->row_array();
-    }
+		$result = $this->db->get_where('registrants', ['id' => $id]);
+		return $result->row_array();
+	}
 
-    //
-    // Register new
-    //
+	//
+	// Create/save new registrant
+	//
 
-    public function register()
-    {
-        $this->load->helper('form');
-        $this->load->library('form_validation');
+	public function create()
+	{
+		// automatically gets the $_POST fields
+		$entry = [
+			"firstname" => 		ucwords(strtolower($this->input->post('firstname'))),
+			"middlename" => 		ucwords(strtolower($this->input->post('middlename'))),
+			"lastname" => 		ucwords(strtolower($this->input->post('lastname'))),
+			"nickname" => 		ucwords(strtolower($this->input->post('nickname'))),
+			"gender" => 		strtolower($this->input->post('gender')),
+			"email" => 			strtolower($this->input->post('email')),
+			"mobile_number" => 	$this->input->post('mobile-number'),
+			"church_name" => 		ucwords(strtolower($this->input->post('church-name'))),
+			"church_address" => 	ucwords(strtolower($this->input->post('church-city'))),
+			"role" => 			strtolower($this->input->post('role'))
+		];
 
-        $this->form_validation->set_rules('lastname', 'Lastname', 'trim|required|min_length[2]|max_length[32]');
-        $this->form_validation->set_rules('firstname', 'Firstname', 'trim|required|min_length[2]|max_length[32]');
-        $this->form_validation->set_rules('middlename', 'Middlename', 'trim|required|min_length[2]|max_length[32]');
-        $this->form_validation->set_rules('gender', 'Gender', 'required');
-        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
-        $this->form_validation->set_rules('mobile-number', 'Mobile number', 'trim|required|min_length[5]');
-        $this->form_validation->set_rules('church-name', 'Church / Organization', 'trim|required|min_length[2]|max_length[100]');
-        $this->form_validation->set_rules('church-city', 'Church / Organization City', 'trim|required|min_length[2]|max_length[200]');
-
-        if ($this->form_validation->run() == FALSE) {
-            // validation is success
-            $this->load->view('pages/events/single');
-        } else {
-            // validation failed
-            $this->load->view('pages/events/reg-success');
-        }
-    }
+		$this->db->insert('registrants', $entry);
+	}
 }
