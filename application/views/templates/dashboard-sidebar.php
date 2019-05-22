cc613f537746926e88227031e22e561000eba311
Axl Joven <axlmorenojoven@gmail.com>
2019-05-22 15:08:16 +0800

    Created the dashboard sidebar

---
+++ application/views/templates/dashboard-sidebar.php
@@ -0,0 +1,27 @@
+<ul>
+      <li class="my-2 mt-0">
+            <a href="dashboard" class="flex items-center font-bold py-2 pt-0">
+                  <i class="text-red-600 mr-2 icon ion-ios-home"></i> Dashboard
+            </a>
+      </li>
+      <li class="my-2">
+            <a href="dashboard/events" class="flex items-center font-bold py-2">
+                  <i class="text-red-600 mr-2 icon ion-ios-calendar"></i> Events
+            </a>
+      </li>
+      <li class="my-2">
+            <a href="dashboard/registrants" class="flex items-center font-bold py-2">
+                  <i class="text-red-600 mr-2 icon ion-ios-people"></i> Registrants
+            </a>
+      </li>
+      <li class="my-2">
+            <a href="dashboard/admins" class="flex items-center font-bold py-2">
+                  <i class="text-red-600 mr-2 icon ion-ios-key"></i> Admins
+            </a>
+      </li>
+      <li class="my-2 mb-0">
+            <a href="dashboard/logout" class="flex items-center font-bold py-2 pb-0">
+                  <i class="text-red-600 mr-2 icon ion-ios-log-out"></i> Logout
+            </a>
+      </li>
+</ul>
